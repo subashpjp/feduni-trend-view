5d100bd603fd7527bd53c95ad95131df19c1c4c0
rajesh132 <nbn132@gmail.com>
2020-04-21 18:56:31 +1000

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './component/header/header.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {SidebarComponent} from './component/sidebar/sidebar.component';
+import {MatButtonModule, MatDividerModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule {
+}
